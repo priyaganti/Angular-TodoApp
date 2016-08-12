b219e13d18a213c4f8244c82dde0aff4e74d6d57
Priyanka Ganti <ganti.priya@gmail.com>
2016-08-12 16:48:45 -0700

    display to-do list using ng-repeat and expressions

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ var app = angular.module('Todo', []);
 // Add a controller
 app.controller('TodoCtrl', function($scope){
   $scope.message = 'Angular is cool';
+  $scope.todos = [
+    'Learn to Paint',
+    'Practice for Half Marathon',
+    'Cook a meal'
+  ];
 });
