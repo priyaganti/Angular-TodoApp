f6de132536338007adeaacfe503a880c2c506ad5
Priyanka Ganti <ganti.priya@gmail.com>
2016-08-12 19:47:04 -0700

    add click eventon done button and remove to-do items on click

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ app.controller('TodoCtrl', function($scope){
     'Practice for Half Marathon',
     'Cook a meal'
   ];
+  $scope.done = function(todo){
+    var chosenIndex = $scope.todos.indexOf(todo);
+    if(chosenIndex !== -1){
+      $scope.todos.splice(chosenIndex, 1); //remove the element from todos array at chosen Index.
+    }
+  };
 });
