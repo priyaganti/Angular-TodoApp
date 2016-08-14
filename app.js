15bc9b8ff98b5e2cb7b741770663523c9a26ea75
Priyanka Ganti <ganti.priya@gmail.com>
2016-08-14 13:23:38 -0700

    add function tocapture new to do item

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ var app = angular.module('Todo', []);
 
 // Add a controller
 app.controller('TodoCtrl', function($scope){
-  $scope.message = 'Angular is cool';
+  $scope.newTodo = '';
   $scope.todos = [
     'Learn to Paint',
     'Practice for Half Marathon',
     'Cook a meal'
   ];
+
+  // Remove to-do item from the list
   $scope.done = function(todo){
     var chosenIndex = $scope.todos.indexOf(todo);
     if(chosenIndex !== -1){
       $scope.todos.splice(chosenIndex, 1); //remove the element from todos array at chosen Index.
+    }
+  };
+
+  //add new to-do item to the list
+   $scope.add = function(e){ //e is the event
+     if(e.which === 13){
+      $scope.todos.push($scope.newTodo);
+      $scope.newTodo = '';
     }
   };
 });
